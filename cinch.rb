8354b630c6c987a2a3eb6fcac854fd215940748e
crshd <crshd@mail.com>
2011-06-07 16:10:09 +0800

    fixed indentify

--- cinch.rb
+++ cinch.rb
@@ -943,7 +943,7 @@ module Plugins
 
     listen_to :connect, method: :identify
     def identify(m)
-      User("nickserv").send("identify" + SECRET)
+      User("nickserv").send("identify " + SECRET)
     end
   end # }}}
 end # }}}
