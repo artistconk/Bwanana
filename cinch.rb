77acca39acc8a953baea384862bde46e7392cdc9
crshd <crshd@mail.com>
2011-08-06 22:22:18 +0800

    removed call to removed plugin

--- cinch.rb
+++ cinch.rb
@@ -5,6 +5,28 @@
 #   @copyright (c) 2010, Christoph Kappel <unexist@dorfelite.net>
 #   @version $Id: bot/cinch.rb,v 208 2011/01/18 20:56:21 unexist $
 
+# Config {{{
+# Server
+SERVER      = "localhost"
+PORT        = 6667
+CHANNEL     = "#Bwanana"
+
+# Bot
+NICK        = "Bwanana"
+SECRET      = "doyoureallythinkiputthisinhere"
+INTERVAL    = 300
+
+# RSS Feed
+FEED        = "http://pixelfuckers.org/submissions.atom"
+
+# Weather.com API
+WEATHER_PAR = "1079693758"
+WEATHER_API = "a6939d9b2b51255c"
+
+# Database
+DBFILE      = "/home/crshd/.config/cinch/database.db"
+# }}}
+
 # Requires
 $:.unshift("#{Dir.pwd}/plugins")
 
@@ -37,28 +59,6 @@ require "todo"
 require "twentyone"
 require "uri"
 require "weather"
-
-# Config {{{
-# Server
-SERVER      = "localhost"
-PORT        = 6667
-CHANNEL     = "#Bwanana"
-
-# Bot
-NICK        = "Bwanana"
-SECRET      = "doyoureallythinkiputthisinhere"
-INTERVAL    = 300
-
-# RSS Feed
-FEED        = "http://pixelfuckers.org/submissions.atom"
-
-# Weather.com API
-WEATHER_PAR = "1079693758"
-WEATHER_API = "a6939d9b2b51255c"
-
-# Database
-DBFILE      = "/home/crshd/.config/cinch/database.db"
-# }}}
 
 # Database {{{
 DataMapper.setup(:default, "sqlite3:///" + DBFILE)
@@ -178,7 +178,6 @@ bot = Cinch::Bot.new do
     Plugins::Uri,
     Plugins::Phrases,
     Plugins::Actions,
-    Plugins::Damnyou,
     Plugins::Submissions,
     Plugins::Google,
     Plugins::Rename,
